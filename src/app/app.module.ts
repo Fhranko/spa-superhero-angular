91141a2e7e2e4dc6414393d469828a859b0e4e37
franco <francohuanaco@gmail.com>
2020-05-13 12:21:29 -0400

    App Finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,10 @@ import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 
 @NgModule({
-	declarations: [ AppComponent, HomeComponent, NavbarComponent, AboutComponent, HeroesComponent, HeroeComponent, BuscadorComponent ],
+	declarations: [ AppComponent, HomeComponent, NavbarComponent, AboutComponent, HeroesComponent, HeroeComponent, BuscadorComponent, HeroeTarjetaComponent ],
 	imports: [ BrowserModule, APP_ROUTING ],
 	providers: [ HeroesService ],
 	bootstrap: [ AppComponent ]
